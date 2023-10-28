ecf7bae001313909f07034755154f995a1c21b24
AhmmedThanveer <thanveerahmmed4@gmail.com>
2023-10-28 13:25:34 +0530

    commit my second changes

--- lib/view/userlist.dart
+++ lib/view/userlist.dart
@@ -13,29 +13,60 @@ class UserList extends StatelessWidget {
   Widget build(BuildContext context) {
     final Users = Provider.of<PostProvider>(context);
 
-    return ListView.builder(
-      itemCount: users.length * 2 - 1,
-      itemBuilder: (context, index) {
-        if (index.isOdd) {
-          final itemIndex = index ~/ 2;
-          return Divider();
-        } else {
-          final itemIndex = index ~/ 2;
-          final user = users[itemIndex];
-          return ListTile(
-            title: Text(user.name),
-            subtitle: Text(user.email),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => UserDetails(user),
-                ),
-              );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User List'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Show the search bar or perform search action
             },
-          );
-        }
-      },
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize:
+              Size.fromHeight(48.0), // Define the height of the search bar
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Search',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.search),
+              ),
+              onChanged: (query) {
+                // Handle search query changes
+                // You can filter the user list based on the search query here
+              },
+            ),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: users.length * 2 - 1,
+        itemBuilder: (context, index) {
+          if (index.isOdd) {
+            final itemIndex = index ~/ 2;
+            return Divider();
+          } else {
+            final itemIndex = index ~/ 2;
+            final user = users[itemIndex];
+            return ListTile(
+              title: Text(user.name),
+              subtitle: Text(user.email),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => UserDetails(user),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
